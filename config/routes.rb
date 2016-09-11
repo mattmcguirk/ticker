2dd4654f782ef04a2845034eb4786eaf97d0497b
Matt McGuirk <matt.mcguirk@gmail.com>
2016-09-11 09:16:40 +0000

    Adds user signup.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get  '/tasks/index', to: 'tasks#index'
   get  '/tasks/view',  to: 'tasks#view'
   get  '/tasks/new',   to: 'tasks#new'
-  post '/tasks/new',   to: 'tasks#create'  
+  post '/tasks/new',   to: 'tasks#create' 
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
   root 'tasks#new'
 
