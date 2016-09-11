efc94a42e38b42caf983b2b765cb4c677c7eea22
Matt McGuirk <matt.mcguirk@gmail.com>
2016-09-11 00:58:04 +0000

    Adds Users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   get  '/tasks', to: 'tasks#index'
   get  '/tasks/index', to: 'tasks#index'
   get  '/tasks/view',  to: 'tasks#view'
   get  '/tasks/new',   to: 'tasks#new'
   post '/tasks/new',   to: 'tasks#create'  
+  resources :users
   root 'tasks#new'
 
 end
