908658ab9fefed8be41fe4e2f38de1305c2d3570
Matt McGuirk <matt.mcguirk@gmail.com>
2016-09-12 02:56:45 +0000

    Adds quips to the homepage.

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -20,4 +20,21 @@ module TasksHelper
     else return number 
   end
 
+  def quip
+    quips = [ 'Enjoy this zen-like calm while it lasts.',
+              'Meditate on nothingness.',
+              'Have you considered a dramatic career change?',
+              'Everyday, give yourself a present.',
+              'Time is an illusion. Lunch time doubly so.',
+              'Dig a hole. Fill it up.',
+              'Cat\'s in the bag. Bag\'s in the river.',
+              'Sometimes you have to roll the hard six.',
+              'If the only thing you have is a fork, everything looks like linguini.',
+              'Confront the spectacle with its own irrelevance',
+              'Saying nothing is a valid response.',
+              'One cloud plus one cloud equals one cloud.'
+            ]
+    quips.shuffle.first
+  end
+
 end
